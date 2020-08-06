ed48ebee1963e87f6e6c58f1db8ae7cef39ade36
josmig <pantigosomiguel59@gmail.com>
2020-08-05 23:51:30 -0500

    datos simples controller ejemplos + despliegue a vistas

--- src/main/java/com/pantigoso/demo/Controller/HomeController.java
+++ src/main/java/com/pantigoso/demo/Controller/HomeController.java
@@ -11,11 +11,17 @@ public class HomeController {
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		var name = "Panti";
-		var mensaje = "Bienvenidos al sistem de mensajeria";		
-		model.addAttribute("titulo", "Home |".concat(name));
-		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("fecha", new Date());
+		
+		String nombre ="Auxiliar de Contabilidad";
+		Date fechaPub = new Date();
+		double salario = 9000.0;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fechaPub", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
+		
 		return "home";
 	}
 
