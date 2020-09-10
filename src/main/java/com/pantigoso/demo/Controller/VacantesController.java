4034767b7c6604dc5bcb78f8cb0ad5032f350689
josmig <pantigosomiguel59@gmail.com>
2020-09-10 18:15:07 -0500

    creacion de botones dinamicos con la anotacion PathVariable para el traslado de la id

--- src/main/java/com/pantigoso/demo/Controller/VacantesController.java
+++ src/main/java/com/pantigoso/demo/Controller/VacantesController.java
@@ -1,4 +1,23 @@
 package com.pantigoso.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/vacantes")
 public class VacantesController {
+	
+	@GetMapping("/ver/{id}")
+	public String verDetalle(@PathVariable("id") int idVacante,Model model) {		
+		System.out.println("IdVacante : " + idVacante);			
+		model.addAttribute("idVacante", idVacante);
+		
+		//Buscar los detalles de la vacante en la BD
+		
+		return "vacantes/detalle";
+	}
+	
 }
